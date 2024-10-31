5812b1ec3885710bbd514cf0eeebf7589013b858
Diogo Antunes <diogoalves2684@gmail.com>
2024-10-31 19:36:01 -0300

    Treino de Thread

--- app/src/main/java/com/example/treinofragment/MainActivity.kt
+++ app/src/main/java/com/example/treinofragment/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.treinofragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.treinofragment.databinding.ActivityMainBinding
 import com.example.treinofragment.fragments.PrimeFragment
 import com.example.treinofragment.fragments.SegundFragment
@@ -17,15 +19,34 @@ class MainActivity : AppCompatActivity() {
 
         binding.btn1.setOnClickListener {
             supportFragmentManager.beginTransaction()
-                .replace( R.id.fragment_CV, PrimeFragment() )
+                .replace( R.id.fragmentCV, PrimeFragment() )
                 .commit()
         }
 
         binding.btn2.setOnClickListener {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_CV, SegundFragment())
+                .replace(R.id.fragmentCV, SegundFragment())
                 .commit()
 
+        }
+        binding.btnThread.setOnClickListener { MinhaThread().start() }
+
+    }
+
+    inner class MinhaThread : Thread() {
+        override fun run() {
+            super.run()
+
+            repeat(15){indece ->
+                Log.i("info_thread", "Executando: $indece T: ${currentThread().name}")
+                runOnUiThread{
+                    Toast.makeText(applicationContext, "Threaddddd", Toast.LENGTH_LONG).show()
+                    Log.i("info_thread", "Executando_no_main: $indece T: ${currentThread().name}")
+                    binding.btnThread.text = "Executando"
+                }
+                sleep(1000) // 1000ms == 1s
+
+            }
         }
     }
 }
